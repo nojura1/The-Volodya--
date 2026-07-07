7c4d1cb8cf64a4e962117c94258b760533877341
nojura1 <dimaloh15967@gmail.com>
2026-07-07 15:09:27 +0200

    fix project structure

--- volodya--/build/Token/TokenNames.d
+++
@@ -1,4 +0,0 @@
-build/Token/TokenNames.o: src/Token/TokenNames.c src/Token/TokenNames.h \
- src/Token/Token.h
-src/Token/TokenNames.h:
-src/Token/Token.h:
--- volodya--/build/Token/TokenVec.d
+++
@@ -1,4 +0,0 @@
-build/Token/TokenVec.o: src/Token/TokenVec.c src/Token/TokenVec.h \
- src/Token/Token.h
-src/Token/TokenVec.h:
-src/Token/Token.h:
